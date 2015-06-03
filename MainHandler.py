5a66738dbaed00103eabff721208a552b2053198
sdpatro <sidharth.patro@outlook.com>
2015-06-03 11:21:58 +0530

    Implemented request handlers for login and sign-up. Form submission is pending.

--- MainHandler.py
+++ MainHandler.py
@@ -1,15 +1,20 @@
 __author__ = 'sdpatro'
 from tornado.ioloop import IOLoop
 from tornado.web import RequestHandler, Application, url
+import SignUpHandler
+import LoginHandler
 
 class MainHandler(RequestHandler):
     def get(self):
-        self.write('<a href="%s">form</a>' %
-                   self.reverse_url("form"))
+        self.write('<h1>Welcome</h1> <br/> Sign up <a href="%s">here</a> and log in <a href="%s">here</a>' % (
+            self.reverse_url("signup"),
+            self.reverse_url("login")))
 
 if __name__ == "__main__":
     app = Application([
-        url(r"/", MainHandler)])
+        url(r"/", MainHandler),
+        url(r"/signup", SignUpHandler.SignUpHandler, name="signup"),
+        url(r"/login", LoginHandler.LoginHandler, name="login")])
 
     app.listen(8888)
     IOLoop.current().start()
